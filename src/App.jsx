33d83c002c604ab65e88ca3b8fe7b140803360c7
Edvin Dardagan <edvin__7@hotmail.com>
2022-11-15 15:25:28 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { questions } from "./questions";
+
 import "./App.css";
+import mobile from "./images/illustration-woman-online-mobile.svg";
+import desktop from "./images/illustration-woman-online-desktop.svg";
+import SingleQuestion from "./SingleQuestion";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [quests] = useState(questions);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + edvin</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <article>
+        <picture>
+          <source media="(min-width: 768px)" srcSet={desktop} />
+          <img src={mobile} alt="" />
+        </picture>
+      </article>
+      <article>
+        <h1>FAQ</h1>
+        {quests.map((quest, index) => (
+          <SingleQuestion key={index} {...quest} />
+        ))}
+      </article>
     </div>
   );
 }
